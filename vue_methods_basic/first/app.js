9771dc16415e325b9e1f77c149e8df9223844273
Aung Min Oo <aungminoo1314@gmail.com>
2023-11-08 20:47:46 +0900

    computed properties in vue

--- vue_methods_basic/first/app.js
+++ vue_methods_basic/first/app.js
@@ -1,15 +1,43 @@
 const app = Vue.createApp({
     data(){
         return{
-            title : "You don't know js",
-            author : "Aung Min Oo",
-            authorAge : 25,
-            showBook : true
+            books : [
+                {
+                    title : "You don't know js",
+                    author : "Aung Min Oo",
+                    authorAge : 25,
+                    img : "../../img/plate1.png",
+                    isFavourite : true
+                },
+                {
+                    title : "You don't know Laravel",
+                    author : "Aung Min Oo",
+                    authorAge : 25,
+                    img : "../../img/plate2.png",
+                    isFavourite : true
+                },
+                {
+                    title : "You don't know Php",
+                    author : "Aung Min Oo",
+                    authorAge : 25,
+                    img : "../../img/plate3.png",
+                    isFavourite : true
+                }
+            ],
+            showBook : true,
+            link : "https://www.google.com",
         }
     },
     methods : {
         changeAuthorName(){
             this.author = "Sayar Aung"
+        }
+    },
+    computed : {
+        filteredBooks (){
+            return this.books.filter(book=>{
+                return book.isFavourite;
+            })
         }
     }
 });
